b701b50e0863054a2279a5c126f740898e3b25fa
Maneesh M <maneesh888@gmail.com>
2023-02-16 11:08:51 +0400

    Project created

---
+++ Composition-Deffable-DEWA/Composition-Deffable-DEWA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Composition-Deffable-DEWA
+//
+//  Created by Maneesh M on 16/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Composition-Deffable-DEWA/Composition-Deffable-DEWAUITests/Composition_Deffable_DEWAUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Composition_Deffable_DEWAUITestsLaunchTests.swift
+//  Composition-Deffable-DEWAUITests
+//
+//  Created by Maneesh M on 16/02/23.
+//
+
+import XCTest
+
+final class Composition_Deffable_DEWAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
